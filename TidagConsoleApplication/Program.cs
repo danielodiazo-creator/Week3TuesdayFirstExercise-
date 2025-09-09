a37c51329253d6def402383f3ba1c283cab67ff4
danielodiazo-creator <danielodiazo@gmail.com>
2025-09-09 12:52:46 +0200

    segundo commit

--- TidagConsoleApplication/Program.cs
+++ TidagConsoleApplication/Program.cs
@@ -10,7 +10,7 @@ namespace TidagConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola");
+            Console.WriteLine("Hola, que tal");
         }
     }
 }
